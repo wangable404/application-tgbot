28bc1adf06c950ee1c6b5796be7e24abc45f8bfa
wangable404 <wangable404@gmail.com>
2026-07-03 19:01:43 +0500

    axios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import express from "express";
-import axios from "axios";
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
