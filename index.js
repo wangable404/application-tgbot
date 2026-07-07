4a1d0731bc8e9951c18f4d7d042eadff38f6a544
wangable404 <wangable404@gmail.com>
2026-07-07 10:15:08 +0500

    text

--- index.js
+++ index.js
@@ -8,36 +8,28 @@ app.use(express.json());
 const {
   TELEGRAM_BOT_TOKEN,
   RELAY_SECRET,
-  MAIN_BACKEND_URL,
+  TG_BACKEND_URL,
+  MAX_BACKEND_URL,
   PORT = 3000,
 } = process.env;
 
 const TG_API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`;
+const MAX_API = "https://platform-api2.max.ru";
 
-app.post("/webhook", async (req, res) => {
+app.post("/tg/webhook", async (req, res) => {
   try {
-    console.log(req.body, 'render tg notify');
-    
-    const response = await axios.post(MAIN_BACKEND_URL, req.body, {
+    const response = await axios.post(TG_BACKEND_URL, req.body, {
       headers: { "x-relay-secret": RELAY_SECRET },
       timeout: 8000,
     });
-
-    console.log('otvet', response);
-    console.log('url', MAIN_BACKEND_URL);
-    
-    
   } catch (err) {
     console.log("forward to main backend failed:", err.message);
   }
   res.sendStatus(200);
 });
 
-app.post("/send", async (req, res) => {
+app.post("/tg/send", async (req, res) => {
   const { secret, chatId, text } = req.body;
-
-  console.log('новое сообщените', chatId, text, secret);
-  
 
   if (secret !== RELAY_SECRET) {
     return res.status(403).json({ error: "forbidden" });
@@ -57,6 +49,34 @@ app.post("/send", async (req, res) => {
     );
     res.status(500).json({ error: err.message });
   }
+});
+
+app.post("/max/webhook", async (req, res) => {
+  try {
+    const response = await axios.post(MAX_BACKEND_URL, req.body, {
+      headers: { "x-relay-secret": RELAY_SECRET },
+      timeout: 8000,
+    });
+  } catch (err) {
+    console.log("forward to main backend failed:", err.message);
+  }
+  res.sendStatus(200);
+});
+
+app.post("/max/send", async (req, res) => {
+  const { secret, text, chatId } = req.body;
+
+  if (secret !== RELAY_SECRET) {
+    return res.status(403).json({ error: "forbidden" });
+  }
+  await axios.post(
+    `${MAX_API}/messages`,
+    { text },
+    {
+      params: { chat_id },
+      headers: { Authorization: TOKEN, "Content-Type": "application/json" },
+    },
+  );
 });
 
 app.get("/", (req, res) => res.send("relay is alive"));
